46c38ecb131a2bf803824f12a899dd312abacae7
Michalinius <michalinius@gmail.com>
2022-09-02 21:26:34 +0200

    task 2 completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const bodyChange = document.querySelector("body");
+const startChange = document.querySelector(`[data-start]`);
+const stopChange = document.querySelector(`[data-stop]`);
+let interval;
+
+function colorAssignment() {
+    bodyChange.style.backgroundColor = getRandomHexColor();
+}
+
+function colorSwitch() {
+    startChange.disabled = true;
+    interval = setInterval (colorAssignment, 1000)
+}
+
+function stopColorSwitch() {
+    startChange.disabled = false;
+    clearInterval(interval);
+}
+
+startChange.addEventListener("click", colorSwitch);
+stopChange.addEventListener("click", stopColorSwitch);
+
 
