588f11f5712afcd9af3288296d5763414c069237
Michalinius <michalinius@gmail.com>
2022-09-03 21:22:34 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector("#search-box");
+
+input.addEventListener("input", debounce(event =>{
+    //tu czyścic
+    if(event.target.value === ""){
+        return
+    }
+
+    fetchCountries(event.target.value).then(data => {
+    if (data.length>10) 
+        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
+    else if (data.length==0) 
+        Notiflix.Notify.failure("Oops, there is no country with that name")
+    else if (data.length==1)
+        console.log('asd')
+    else if (data.length >= 2 && data.length <= 10)
+        console.log('asdd')
+    })
+    
+},DEBOUNCE_DELAY))
