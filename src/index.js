6dbd37965b187d20e7a353c24daf5b260e77ccec
Michalinius <michalinius@gmail.com>
2022-09-05 18:27:34 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,27 +1,124 @@
-import './css/styles.css';
-import { fetchCountries } from './fetchCountries';
-import debounce from 'lodash.debounce';
-import Notiflix from 'notiflix';
+import axios from "axios";
+import Notiflix from "notiflix";
 
-const DEBOUNCE_DELAY = 300;
 
-const input = document.querySelector("#search-box");
+const form = document.querySelector(`form`);
+const input = document.querySelector(`[name="searchQuery"]`)
+const mainDiv = document.querySelector(".gallery")
+const button = document.querySelector(".load-more")
+button.style.display = "none";
+let inputValue;
+let page = 1;
+console.log(mainDiv);
 
-input.addEventListener("input", debounce(event =>{
-    //tu czyścic
-    if(event.target.value === ""){
-        return
+console.log(input.value)
+
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    console.log(input.value)
+    if (!input.value) {
+        mainDiv.innerHTML = "";
+        button.style.display = "none";
     }
+    else {
+        page = 1;
+        axios.get(`https://pixabay.com/api/?key=29678307-c5737503ea3de173bef225585&q=${input.value.trim()}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`)
+            .then(res => res.data)
+            .then(data => {
+                inputValue = input.value;
+                const newData = data.hits.map(results => {
+                    return {
+                        webformatURL: results.webformatURL,
+                        largeImageURL: results.largeImageURL,
+                        alt: results.tags,
+                        likes: results.likes,
+                        views: results.views,
+                        comments: results.comments,
+                        downloads: results.downloads
+                    }
+                }
+                )
+                if (newData.length == 0) Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+                else {
+                    console.log(newData);
+                    mainDiv.innerHTML = "";
+                    for (i = 1; i < newData.length; i++) {
+                        mainDiv.insertAdjacentHTML("beforeend", `<div class="single-image">
+                    <div class="image-box"><img class="image" src="${newData[i].webformatURL}" alt="${newData[i].alt}"></div>
+                <div class="info">
+                    <span class="info-box">
+                            <b>Likes</b>
+                            <p>${newData[i].likes}</p>
+                        </span>
+                        <span class="info-box">
+                            <b>Views</b>
+                            <p>${newData[i].views}</p>
+                        </span>
+                        <span class="info-box">
+                            <b>Comments</b>
+                            <p>${newData[i].comments}</p>
+                        </span>
+                        <span class="info-box">
+                            <b>Downloads</b>
+                            <p>${newData[i].downloads}</p>
+                        </span></div> 
+                    </div>`)
+                    }
+                    button.style.display = "block";
+                }
+            })
+            .catch(error => console.warn(error))
+    }
+})
 
-    fetchCountries(event.target.value).then(data => {
-    if (data.length>10) 
-        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
-    else if (data.length==0) 
-        Notiflix.Notify.failure("Oops, there is no country with that name")
-    else if (data.length==1)
-        console.log('asd')
-    else if (data.length >= 2 && data.length <= 10)
-        console.log('asdd')
-    })
-    
-},DEBOUNCE_DELAY))
+button.addEventListener("click", () => {
+
+    page += 1;
+    console.log(page)
+    axios.get(`https://pixabay.com/api/?key=29678307-c5737503ea3de173bef225585&q=${inputValue.trim()}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`)
+        .then(res => res.data)
+        .then(data => {
+            if (data.totalHits / 40 > page) {
+                console.log(data)
+                const newData = data.hits.map(results => {
+                    return {
+                        webformatURL: results.webformatURL,
+                        largeImageURL: results.largeImageURL,
+                        alt: results.tags,
+                        likes: results.likes,
+                        views: results.views,
+                        comments: results.comments,
+                        downloads: results.downloads
+                    }
+                })
+                for (i = 1; i < newData.length; i++) {
+                    mainDiv.insertAdjacentHTML("beforeend", `<div class="single-image">
+            <div class="image-box"><img class="image" src="${newData[i].webformatURL}" alt="${newData[i].alt}"></div>
+        <div class="info">
+            <span class="info-box">
+                    <b>Likes</b>
+                    <p>${newData[i].likes}</p>
+                </span>
+                <span class="info-box">
+                    <b>Views</b>
+                    <p>${newData[i].views}</p>
+                </span>
+                <span class="info-box">
+                    <b>Comments</b>
+                    <p>${newData[i].comments}</p>
+                </span>
+                <span class="info-box">
+                    <b>Downloads</b>
+                    <p>${newData[i].downloads}</p>
+                </span></div> 
+            </div>`)
+                }
+            }
+            else {
+                Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+                button.style.display = "none";
+            }
+        })
+        .catch(error => console.warn(error))
+})
